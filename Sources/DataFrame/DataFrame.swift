83b0c51a609c2569c217ad87585c154f7f5eb36d
Max Desiatov <max.desiatov@gmail.com>
2018-02-23 21:01:00 +0000

    Added empty DataFrame constructor, Row and DataFrameGroup structs

--- Sources/DataFrame/DataFrame.swift
+++ Sources/DataFrame/DataFrame.swift
@@ -1,13 +1,16 @@
-struct IntDataFrame<C> where C: Hashable {
-  private var wrapped: DataFrame<C, Int>
-}
-
 struct DataFrame<C, R> where C: Hashable, R: Hashable {
-  private var columns = [C: [Any]]()
-  private var rowIndex = [R: Int]()
+  fileprivate var columns = [C: [Any]]()
+  fileprivate var rowIndex = [R: Int]()
+
+  /// Empty DataFrame
+  init() {
+  }
 
   func set(column: C, values: [Any], index: [R]) {
-    
+    // Autogenerate missing Int row index
+    if C.self == Int.self {
+      print("test")
+    }
   }
 
   subscript(index: C) -> [Any]? {
@@ -16,12 +19,37 @@ struct DataFrame<C, R> where C: Hashable, R: Hashable {
     }
   }
 
-  subscript(index: R) -> [C: Any]? {
+  subscript(index: R) -> Row<C, R>? {
     get {
-      guard let row = rowIndex[index] else { return nil }
+      guard let rowPosition = rowIndex[index] else { return nil }
 
-      return columns.mapValues { $0[row] }
+      return Row(dataFrame: self, rowPosition: rowPosition)
+    }
+  }
+
+  func group<G>(by: @escaping (Row<C, R>) -> G) -> DataFrameGroup<C, R, G>
+  where G: Equatable {
+    return DataFrameGroup(grouping: by)
+  }
+}
+
+// This should encapsulate DataFrame access without a need to copy every row
+// to a dictionary, although need to test how Array and Dictionary COW works here.
+// Given that a Row has `dataFrame` property, when does all `dataFrame` contents
+// get copied to a Row, or is it copied at all if the original DataFrame is
+// not modified?
+// Is a separate Row struct needed, or could it just wrap something like DataSlice?
+struct Row<C, R> where C: Hashable, R: Hashable {
+  fileprivate let dataFrame: DataFrame<C, R>
+  fileprivate let rowPosition: Int
+
+  subscript(index: C) -> Any? {
+    get {
+      return dataFrame[index]?[rowPosition]
     }
   }
 }
 
+struct DataFrameGroup<C, R, G> where C: Hashable, R: Hashable, G: Equatable {
+  let grouping: (Row<C, R>) -> G
+}
